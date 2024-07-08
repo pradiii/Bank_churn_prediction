529a92561669d7d3cab617792aeb86921f7c4834
pradeep-b <pradeep.b453@gmail.com>
2024-07-08 19:51:44 +0530

    added content to req file and setup.py file

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_DOT_E = "-e ."
+def get_requirements(file_path:str)->List[str]:
+
+    requirements = []
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n"," ") for req in requirements]
+
+        if HYPEN_DOT_E in requirements:
+            requirements.remove(HYPEN_DOT_E)
+
+    return requirements
+
+setup(
+    name="bank_churn_prediction",
+    version="0.0.1",
+    author="Pradeep",
+    author_email="pradeep.b453@gmail.com",
+    packages=find_packages(),
+    install_requires = get_requirements("requirements.txt")
+)
